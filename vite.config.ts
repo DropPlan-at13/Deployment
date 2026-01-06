8305a468e8a9dc8fc0bdea55aacb7660e5879649
Aathish <63100842+theaathish@users.noreply.github.com>
2026-01-06 19:57:34 +0530

    fix the base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Match the GitHub Pages repo path without a leading slash
-  base: 'Deployment/',
+  base: '/',
   plugins: [react()],
 });
